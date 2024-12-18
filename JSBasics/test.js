fe5ad729478c1cfe9f2c237d43fe55afee5296cf
Ayush-pailoss <155214960+Ayush-pailoss@users.noreply.github.com>
2024-12-18 15:58:23 +0000

    datatypes

--- JSBasics/test.js
+++ JSBasics/test.js
@@ -1,1 +1,11 @@
-console.log("hello"); 
+const id = 11111;
+let firstName = " ayush";
+var lastName =" pailoss";
+address = " nichar"
+let accountState;
+
+firstName = "wakil"
+lastName = "pailosss"
+address = "nichar"
+
+console.table([firstName,lastName,address,id,accountState])
